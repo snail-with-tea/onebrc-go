d69098e46c9370d45512195db487217eb1a71dff
DimonNeAdekvat <tipounlogick@gmail.com>
2024-02-25 22:41:01 +0300

    Added remote origin

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
+// This is basic onebrc implementation.
+// I will try to improve it in past
 package main
 
 import "fmt"
-import g "github.com/snail-with-tea/onebrc-go/generator"
 
-// This is test doc
 func main() {
 	fmt.Println("Let's start sorting!")
-	g.Start()
 }
