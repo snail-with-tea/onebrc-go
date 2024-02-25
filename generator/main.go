737f8bd83292f29c1d2f44f34be1ab82c4fca710
DimonNeAdekvat <tipounlogick@gmail.com>
2024-02-26 00:25:02 +0300

    Let's write it to remote

--- generator/main.go
+++ generator/main.go
@@ -1,6 +1,26 @@
 package generator
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var GenSize string
+
+func init() {
+	const (
+		deflt = "50k"
+		usage = "set generation size;"
+	)
+	flag.StringVar(&GenSize, "genSize", deflt, usage)
+	flag.StringVar(&GenSize, "S", deflt, usage+" (shorthand)")
+}
+
+func main() {
+	fmt.Println("Let's generate some files")
+	flag.Parse()
+	fmt.Println(GenSize)
+}
 
 func Start() {
 	fmt.Println("Let's generate some files")
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ package main
 
 import "fmt"
 
+// import g "github.com/snail-with-tea/onebrc-go/generator"
+
 func main() {
 	fmt.Println("Let's start sorting!")
 }
